5ca04f5e1516dafb94c1015fb535d8c38b60bbf4
Bartu Atabek <32511928+bartuatabek@users.noreply.github.com>
2018-01-26 22:47:16 +0300

    Add course pane

---
+++ CoursePane/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = new AnchorPane();
+			root.getChildren().add(new CoursePane("CS 201"));
+			root.setStyle("-fx-background-color: black;");
+			Scene scene = new Scene(root,360,680);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
